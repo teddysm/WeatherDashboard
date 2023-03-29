2a1e79f714c34a201a760d58027bb9f8bb8ebdd9
Thanh Doan <doan.teddy@gmail.com>
2023-03-29 19:41:26 -0400

    add cards and page load bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,33 @@
 const APIKey = "f6d2936051e6180de54a732ae46a3ab1"; // units=imperial
 let city = "Atlanta";
 
-pageLoad();
-async function pageLoad(event) {
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey + '&units=imperial';
-  var response = await fetch(requestUrl);
-  var data = await response.json();
-  $("#current-city").text(data.city.name);
-  $("#temp").text("Current Temperature\n" + data.list[0].main.temp + " °F");
-  $("#wind").text("Current Wind Speed\n" + data.list[0].wind.speed + " MPH");
-  $("#humid").text("Current Humidity\n" + data.list[0].main.humidity + " %");
-}
+window.addEventListener("load", (event)=> {
+  event.preventDefault();
+  getApi(event);
+})
 
 
 $("#weather-search").submit(getApi);
 // $("#check").click(getApi);
-
 async function getApi(event) {
   event.preventDefault();
-  if ($("#search-box").val()) city = $("#search-box").val();
-  console.log(city);
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey + '&units=imperial';
-  var response = await fetch(requestUrl);
-  var data = await response.json();
-  $("#current-city").text(data.city.name);
-  $("#temp").text("Current Temperature\n" + data.list[0].main.temp + " °F");
-  $("#wind").text("Current Wind Speed\n" + data.list[0].wind.speed + " MPH");
-  $("#humid").text("Current Humidity\n" + data.list[0].main.humidity + " %");
+  if ($("#search-box").val() || city === "Atlanta"){
+    //$("#search-box").val() !== "" ? city = $("#search-box").val() : city;
+    if($("#search-box").val() !== "") {
+      city = $("#search-box").val();
+    }
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey + '&units=imperial';
+    var response = await fetch(requestUrl);
+    var data = await response.json();
+    $("#current-city").text(data.city.name);
+    $("#temp").text(data.list[0].main.temp + " °F");
+    $("#wind").text(data.list[0].wind.speed + " MPH");
+    $("#humid").text(data.list[0].main.humidity + " %");
+  } else{
+    alert("Please enter a city!");
+    return;
+  }
 }
-
-
 
 
 function setTimeHeader(){
@@ -54,7 +52,7 @@ $( function() {
     "Singapore", "Paris", "London", "Dubai", "Istanbul", "Delhi", "Kuala Lumpur", "Shenzhen", "Mumbai", "Phuket",
     "Rome", "Tokyo", "Taipei", "Guangzhou", "Prague", "	Seoul", "Amsterdam", "Osaka", "Shanghai", "Barcelona",
     "Milan", "Vienna", "Cancún", "Berlin", "Cairo", "Moscow", "Venice", "Madrid", "Ha Long", "Toronto", "Dublin",
-    "Jerusalem", "Sydney", "Munich", "Beijing", "Jakarta", "Budapest", "Lisbon", "Rhodes", "Heraklion", "Vancouver"
+    "Jerusalem", "Sydney", "Munich", "Beijing", "Jakarta", "Budapest", "Lisbon", "Rhodes", "Athens", "Vancouver"
   ];
   $("#search-box").autocomplete({
     source: availableCities
