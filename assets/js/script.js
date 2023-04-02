10920a2a2f7b688bcc15087db83dadc6d1234fd0
Thanh Doan <doan.teddy@gmail.com>
2023-04-01 21:59:49 -0400

    bug fixes 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const APIKey = "f6d2936051e6180de54a732ae46a3ab1"; // units=imperial
 let city = "Atlanta";
 
+
 window.addEventListener("load", (event)=> {
   event.preventDefault();
   getApi(event);
@@ -12,7 +13,8 @@ $("#weather-search").submit(getApi);
 async function getApi(event) {
   let today = dayjs().format('MMM DD, YYYY'); 
   event.preventDefault();
-  if ($("#search-box").val() || city === "Atlanta"){
+  // city === "Atlanta" city !== ""
+  if ($("#search-box").val() || city !== ""){
     $("#search-box").val() !== "" ? city = $("#search-box").val().trim() : city;
     // if($("#search-box").val() !== "") {
     //   city = $("#search-box").val().trim();
@@ -20,20 +22,24 @@ async function getApi(event) {
     var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey + '&units=imperial';
     var response = await fetch(requestUrl);
     var data = await response.json();
-    handleFiveDay(data.list);
+    console.log(data.list);
+    
     $("#current-city").text(data.city.name + " " + today);
     $("#temp").text(data.list[0].main.temp + " °F");
     $("#wind").text(data.list[0].wind.speed + " MPH");
-    $("#humid").text(data.list[0].main.humidity + " %");   
+    $("#humid").text(data.list[0].main.humidity + " %");  
+    handleFiveDay(data.list); 
   } else{
     alert("Please enter a city!");
     return;
   }
 }
 
-
+// <button type="button" class="btn history-btn btn-info mt-1 mb-3">Paris</button>
 function handleFiveDay(x){
-  for(let i = 7; i < x.length; i+=7){
+  // TODO: Work on tomorrow
+  let tomorrow = dayjs().add(1, 'day').format('MMM DD, YYYY');
+  for(let i = 6; i < x.length; i+=8){
     $(`#five-date-${i}`).text(dayjs(x[i].dt_txt.split(" ")[0]).format("MMM-DD-YYYY"));
     $(`#five-day-temp-${i}`).text(`${x[i].main.temp} °F`);
     $(`#five-day-wind-${i}`).text(`${x[i].wind.speed} MPH`);
