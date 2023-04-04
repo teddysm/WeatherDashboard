4895296ba574c77e341e0e577561c60cad720199
Thanh Doan <doan.teddy@gmail.com>
2023-04-03 20:30:41 -0400

    adding comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
 const APIKey = "f6d2936051e6180de54a732ae46a3ab1"; 
 let city = "Atlanta";
 
-
+// make an API call on page load with the default city of Atlanta
 window.addEventListener("load", (event)=> {
   event.preventDefault();
   getApi(event);
 })
 
+// clear local storage on page load
 window.addEventListener("unload", (event)=> {
   event.preventDefault();
   localStorage.clear();
 })
 
-
+// whenever user clicks on the submit button, make an API call
 $("#weather-search").submit(getApi);
 
 async function getApi(event) {
   event.preventDefault();
   let today = dayjs().format('MMM DD, YYYY'); 
+  // if there is text in the search box, use that text for the city
   if ($("#search-box").val() || city !== ""){
     $("#search-box").val() !== "" ? city = $("#search-box").val().trim() : city;
     createHistoryBtn(city);
@@ -28,7 +30,6 @@ async function getApi(event) {
     let iconURL = "https://openweathermap.org/img/wn/" + icon +".png";
     let iconElement = $('<img>').attr('src', iconURL).attr('width', '45').attr('height', '45');
 
-    // TODO: ICON
     $("#current-city").text(data.city.name + " " + today);
     $('#status').text(data.list[0].weather[0].description).append(iconElement);
     $("#temp").text(data.list[0].main.temp + " °F");
@@ -36,12 +37,16 @@ async function getApi(event) {
     $("#humid").text(data.list[0].main.humidity + " %");  
     handleFiveDay(data.list); 
     $("#search-box").val("");
+  
+  // if the text box is empty, throw an alert
   } else{
     alert("Please enter a city!");
     return;
   }
 }
 
+// check local storage to see if the city parameter is already in there
+// if not then we add that city to local storage and add a button for that city
 function createHistoryBtn(city){
   if(!localStorage.getItem(city)){
     localStorage.setItem(city, city);
@@ -53,7 +58,7 @@ function createHistoryBtn(city){
   }
 }
 
-
+// create an event listener for the history buttons
 $(".history-btn-container").on("click", function(event){
   if(event.target.innerText){
     city = event.target.innerText;
@@ -62,7 +67,7 @@ $(".history-btn-container").on("click", function(event){
 })
 
 
-
+// generate information for the 5 day forecast cards
 function handleFiveDay(input){
   for(let i = 6; i < input.length; i+=8){
     let icon = input[i].weather[0].icon;
@@ -76,7 +81,7 @@ function handleFiveDay(input){
   }
 }
 
-
+// set the time header and make time live
 function setTimeHeader(){
   let today = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a'); 
   $("#current-time").text(today);
@@ -90,6 +95,7 @@ function setTimeHeader(){
 setTimeHeader();
 
 
+// add autocomplete
 $( function() {
   var availableCities = [
     "Atlanta", "Las Vegas", "New York", "Los Angeles", "Seattle", "Lisbon", "Nashville",
